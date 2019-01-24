1e9a1b6b514038c4f9117d37255e9ab0e65fb621
beginmani <manishi.sharma98@gmail.com>
2019-01-24 12:22:21 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React,{ Component} from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
+import LoginForm from './components/LoginForm';
+import ReduxThunk from 'redux-thunk';
+
+class App extends Component{
+    componentWillMount(){
+        const config = {
+            apiKey: 'AIzaSyAsT3tdsDESKJuo8eiv2gy0o2KPPuka6qk',
+            authDomain: 'manager-35357.firebaseapp.com',
+            databaseURL: 'https://manager-35357.firebaseio.com',
+            projectId: 'manager-35357',
+            storageBucket: 'manager-35357.appspot.com',
+            messagingSenderId: '589977811046'
+          };
+          firebase.initializeApp(config);
+    }
+    render(){
+        const store = createStore(reducers,{},applyMiddleware(ReduxThunk));
+        return(
+            <Provider store={store}>
+                <LoginForm />
+            </Provider>
+        );
+    }
+}
+
+export default App;
