07b7a7826637c4ede9c1a197c141d09fe424adaa
beginmani <manishi.sharma98@gmail.com>
2019-01-24 12:44:56 +0530

    email

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -47,7 +47,7 @@ class LoginForm extends Component{
                <CardSection>
                  <Input 
                  label="Email"
-                 placeholder="email@gmail.com"
+                 placeholder="email@mail.com"
                  onChangeText={this.onEmailChange.bind(this)}
                  value={this.props.email}
                  />
