3d0c532f9cecdce275edb0f45f93d632f557d6b1
beginmani <manishi.sharma98@gmail.com>
2019-01-27 12:15:52 +0530

    Adding picker component

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,4 +1,5 @@
 import React ,{Component} from 'react';
+import {Picker ,Text} from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate} from '../actions';
 import { Card,CardSection,Input,Button} from './common';
@@ -26,7 +27,21 @@ class EmployeeCreate extends Component {
                    />
                  </CardSection>
 
-                <CardSection>
+                <CardSection style={{flexDirection: 'coloumn'}}>
+                    <Text style={StyleSheet.pickerTextStyle}>Shift</Text>
+                    <Picker
+                       style={{flex:1}}
+                      selectedValue={this.props.shift}
+                      onValueChange={value => this.props.employeeUpdate({ prop:'shift',value})}
+                    >
+                        <Picker.Item label="Monday" value="Monday" />
+                        <Picker.Item label="Tuesday" value="Tuesday" />
+                        <Picker.Item label="Wednesday" value="Wednesday" />
+                        <Picker.Item label="Thursday" value="Thursday" />
+                        <Picker.Item label="Friday" value="Friday" />
+                        <Picker.Item label="Saturday" value="Saturday" />
+                        <Picker.Item label="Sunday" value="Sunday" />
+                    </Picker>
                 </CardSection>
 
                 <CardSection>
@@ -38,6 +53,12 @@ class EmployeeCreate extends Component {
         );
     }
 }
+const styles={
+    pickerTextStyle: {
+        fontSize: 18,
+        paddingLeft: 20
+    }
+};
 
 const mapStateToProps=(state) => {
     const{ name,phone,shift} =state.employeeForm;
