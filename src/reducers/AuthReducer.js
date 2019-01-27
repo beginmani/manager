a9610b201d5463ecbdda2d5bcae684945015385c
beginmani <manishi.sharma98@gmail.com>
2019-01-27 10:11:38 +0530

    Login Page working

--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -26,7 +26,7 @@ export default (state=INITIAL_STATE,action) => {
             return {...state,...INITIAL_STATE,user:action.payload,};
                
         case LOGIN_USER_FAIL:
-            return {...state,error:'Authentication Failed',password:'',loadin:false};
+            return {...state,error:'Authentication Failed',password:'',loading:false};
 
         default:
          return state;
